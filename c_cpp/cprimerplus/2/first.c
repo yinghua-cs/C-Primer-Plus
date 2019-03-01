dbc90eb3f1a77ab3569bec558825dfb1a920c696
Yinghua Han <hanying@indiana.edu>
2019-03-01 11:17:20 -0500

    Listing 2.1 in Chapter 2, C primer plus, 6th. uploaded.

---
+++ c_cpp/cprimerplus/2/first.c
@@ -0,0 +1,21 @@
+//
+//  first.c
+//  - Listing 2.1, Chapter 2, C Primer Plus, 6th.
+//
+//  Created by Yinghua Han on 1/30/19.
+//  Copyright © 2019 Yinghua Han. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+	int num;
+	num = 1;
+
+	printf("I am a simple ");
+	printf("computer. \n");
+	printf("My favorite number is %d because it is first.\n");
+
+	return 0;
+}
