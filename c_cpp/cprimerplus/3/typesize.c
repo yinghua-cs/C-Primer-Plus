71d1c852c833e7f889bb1d30b61d1b9046ecbf36
Yinghua Han <hanying@indiana.edu>
2020-04-06 01:50:36 -0700

    Example for the difference between char and unsigned char.

--- c_cpp/cprimerplus/3/typesize.c
+++ c_cpp/cprimerplus/3/typesize.c
@@ -1,14 +1,18 @@
-//  Created by Yinghua Han on 1/15/2020.
+//  Created by Yinghua Han on 4/6/2020.
 //  Copyright © 2020 Yinghua Han. All rights reserved.
 
 # include <stdio.h>
 
-int main(void){
-   int a;
-   long b;
-   float f;
-   double d;
-   char c;
-   printf("\nint:%d\nlong:%d\nfloat:%d\ndouble:%d\nchar:%d\n",sizeof(a),sizeof(b),sizeof(f),sizeof(d),sizeof(c));
+int main(void)
+{
+
+   printf("\n");
+   printf("char:%lu\n", sizeof(char));	// %zd for C99 and C11
+   printf("short:%lu\n", sizeof(short));
+   printf("int:%lu\n", sizeof(int));
+   printf("long:%lu\n", sizeof(long));
+   printf("float:%lu\n", sizeof(float));
+   printf("double:%lu\n", sizeof(double));
+   
    return 0;
 }
---
+++ c_cpp/cprimerplus/3/unsignedchar.c
@@ -0,0 +1,51 @@
+// from https://www.cnblogs.com/qytan36/archive/2010/09/27/1836569.html
+// the difference between type "char" and "unsigned char"
+
+#include <stdio.h>
+
+void func(unsigned char value)
+{
+	char c = value;
+	unsigned char uc = value;
+	unsigned int a = c, b = uc;
+	int i = c, j = uc;
+
+	printf("----------------------------\n");
+	printf("\tchar\t\tunsigned char\n");
+	printf("%%c:\t%c\t\t%c\n", c, uc);
+	printf("%%x:\t%#x\t\t%#x\n", c, uc);
+	printf("%%u:\t%u(%#x)\t\t%u\n", a, a, b);
+	printf("%%d:\t%d\t\t%d\n", i, j);
+	printf("\n");
+
+}
+
+int main(void)
+{
+	func(0x80);	// binary: 1000 0000
+	// Output:
+	// ----------------------------
+	// 	char					unsigned char
+	// %c:	?						?
+	// %x:	0xffffff80				0x80
+	// %u:	4294967168(0xffffff80)	128
+	// %d:	-128					128
+	// **********************************
+	// 最高位是1，signed char赋值到int时会做最高位(符号位)扩展，
+	// 出现意想不到的问题。
+
+
+	func(0x7f);	// binary: 0111 1111
+	// Output:
+	// ----------------------------
+	// 	char		unsigned char
+	// %c:			
+	// %x:	0x7f		0x7f
+	// %u:	127(0x7f)	127
+	// %d:	127			127
+	// *****************************
+	// 最高位是0，故即使signed char赋值到int时做最高位扩展也没出错。
+
+	return 0;
+
+}
